7e84c84c8bf1977fc613cef655d1eaf03691dbf4
Nihal Chollampat <nihal17122004@gmail.com>
2024-12-09 15:30:10 +0530

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -26,31 +26,9 @@ function DisplayData() {
 fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-        // console.log(data)
+
         arr = data
 
         console.log("arr:", arr)
         DisplayData()
     })
-
-
-// function add() {
-//     const NameField = document.getElementById('name')
-//     const PointsField = document.getElementById('points')
-
-//     // let data = {
-//     //     Id: Date.now(),
-//     //     "Name": NameField.value,
-//     //     "Points": PointsField.value,
-//     // }
-
-//     // console.log(data)
-
-//     // arr.push(data)
-//     // arr.sort()
-
-
-
-//     DisplayData()
-// }
-
